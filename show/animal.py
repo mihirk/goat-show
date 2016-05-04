4004b9d725ed1fdf0cea954296e90b20390e877b
mmihirk <mihir.khatwani@gmail.com>
2016-05-04 18:28:02 +0100

    Something

--- show/animal.py
+++ show/animal.py
@@ -2,6 +2,9 @@ class Animal(object):
     def __init__(self, name):
         self.name = name
 
+    def can_perform(self, day):
+        pass
+
     @classmethod
     def factory(cls, animal_data):
         return cls(**animal_data)
